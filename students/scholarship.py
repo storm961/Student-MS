78b15b63ba7d92ea13713490ba5a168db4b73cee
Jeffry-N <116834942+Jeffry-N@users.noreply.github.com>
2025-01-28 15:43:42 +0200

    Fixed a bug

--- students/scholarship.py
+++ students/scholarship.py
@@ -9,20 +9,21 @@ connection = mysql.connector.connect(
 
 def check_scholarship():
     if not connection.is_connected():
-        print("Could not connect to the database. ")
+        print("Could not connect to the database.")
         return
+
     cursor = connection.cursor()
 
-    student_id = int(input("Enter the studnent's ID to check for scholarship status: "))
+    student_id = int(input("Enter the student ID to check scholarship: "))
 
     sql_query = "SELECT first_name, last_name, gpa FROM students WHERE student_id = %s"
-
-    cursor.execute(sql_query, (student_id))
+    cursor.execute(sql_query, (student_id,))
     student = cursor.fetchone()
 
     if not student:
         print("Student not found!")
         return
+
     first_name, last_name, gpa = student
 
     scholarship_percentage = 0
@@ -35,16 +36,16 @@ def check_scholarship():
     elif gpa >= 2.5:
         scholarship_percentage = 20
 
-    print(f"Student: {first_name} {last_name}")
+    print(f"\nStudent: {first_name} {last_name}")
     print(f"GPA: {gpa:.2f}")
-
     if scholarship_percentage > 0:
-        print(f"Scholarship percentage: {scholarship_percentage}")
+        print(f"Scholarship Percentage: {scholarship_percentage}%")
     else:
-        print(f"No scholarship available for this student.")
+        print("No scholarship available for this GPA.")
 
     cursor.close()
 
 if __name__ == "__main__":
     check_scholarship()
+
 connection.close()
